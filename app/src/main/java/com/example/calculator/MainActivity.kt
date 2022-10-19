4ff251b745b8d22445354a988f50491b0fb343f9
AnimolPradeep <115970321+AnimolPradeep@users.noreply.github.com>
2022-10-19 19:17:28 +0530

    Made some edits for readability

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var result: EditText
@@ -14,27 +14,17 @@ class MainActivity : AppCompatActivity() {
     private var operand2: Double? = 0.0
     private var pendingOperation = "="
 
-    private val displayOperation by lazy { findViewById<TextView>(R.id.operation) }
+    private lateinit var binding: ActivityMainBinding
+
+    private val displayOperation by lazy { binding.operation }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         result = findViewById(R.id.result)
         newNumber = findViewById(R.id.newNum)
-
-        //data input buttons
-        val btn0: Button = findViewById(R.id.btn0)
-        val btn1: Button = findViewById(R.id.btn1)
-        val btn2: Button = findViewById(R.id.btn2)
-        val btn3: Button = findViewById(R.id.btn3)
-        val btn4: Button = findViewById(R.id.btn4)
-        val btn5: Button = findViewById(R.id.btn5)
-        val btn6: Button = findViewById(R.id.btn6)
-        val btn7: Button = findViewById(R.id.btn7)
-        val btn8: Button = findViewById(R.id.btn8)
-        val btn9: Button = findViewById(R.id.btn9)
-        val dot: Button = findViewById(R.id.dot)
-        val neg: Button = findViewById(R.id.neg)
 
         //operation buttons
         val equals = findViewById<Button>(R.id.equals)
@@ -48,59 +38,66 @@ class MainActivity : AppCompatActivity() {
             newNumber.append(b.text)
         }
 
-        btn0.setOnClickListener(listener)
-        btn1.setOnClickListener(listener)
-        btn2.setOnClickListener(listener)
-        btn3.setOnClickListener(listener)
-        btn4.setOnClickListener(listener)
-        btn5.setOnClickListener(listener)
-        btn6.setOnClickListener(listener)
-        btn7.setOnClickListener(listener)
-        btn8.setOnClickListener(listener)
-        btn9.setOnClickListener(listener)
-        dot.setOnClickListener(listener)
+        binding.apply {
+            btn0.setOnClickListener(listener)
+            btn1.setOnClickListener(listener)
+            btn2.setOnClickListener(listener)
+            btn3.setOnClickListener(listener)
+            btn4.setOnClickListener(listener)
+            btn5.setOnClickListener(listener)
+            btn6.setOnClickListener(listener)
+            btn7.setOnClickListener(listener)
+            btn8.setOnClickListener(listener)
+            btn9.setOnClickListener(listener)
+            dot.setOnClickListener(listener)
+        }
 
-        val opListener = View.OnClickListener {
+        val opListener = View.OnClickListener { v ->
             val op = (v as Button).text.toString()
             val value = newNumber.text.toString()
-            if (value.isNotEmpty()){
+            if (value.isNotEmpty()) {
                 performOperation(value, op)
             }
             pendingOperation = op
-            displayOperation.text= pendingOperation
-
+            displayOperation.text = pendingOperation
 
 
         }
-        equals.setOnClickListener(opListener)
-        div.setOnClickListener(opListener)
-        mul.setOnClickListener(opListener)
-        sub.setOnClickListener(opListener)
-        add.setOnClickListener(opListener)
 
-        neg.setOnClickListener({ view ->
-            val value =newNumber.text.toString()
-            if (value.isEmpty()){
-                newNumber.setText("-")
+        binding.apply {
+            equals.setOnClickListener(opListener)
+            div.setOnClickListener(opListener)
+            mul.setOnClickListener(opListener)
+            sub.setOnClickListener(opListener)
+            add.setOnClickListener(opListener)
+            neg.setOnClickListener {
+                val value = newNumber.text.toString()
+                if (value.isEmpty()) {
+                    newNumber.setText("-")
+                }
             }
-        })
+        }
+
 
     }
-    private fun performOperation(value: String, operation: String){
-        if (operand1 == null){
+
+    private fun performOperation(value: String, operation: String) {
+        if (operand1 == null) {
             operand1 = value.toDouble()
-        }else{
+        } else {
             operand2 = value.toDouble()
-            if (pendingOperation == "="){
+            if (pendingOperation == "=") {
                 pendingOperation = operation
             }
-            when (pendingOperation){
+            when (pendingOperation) {
                 "=" -> operand1 = operand2
-                "/" -> operand1 = if (operand2 == 0.0){
-                             Double.NaN
-                        }else{
-                            operand1!! / operand2!!
-                         }
+
+
+                "/" -> operand1 = if (operand2 == 0.0) {
+                    Double.NaN
+                } else {
+                    operand1!! / operand2!!
+                }
                 "*" -> operand1 = operand1!! * operand2!!
                 "-" -> operand1 = operand1!! - operand2!!
                 "+" -> operand1 = operand1!! + operand2!!
